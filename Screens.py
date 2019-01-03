7ddc12df1b78e49f1e8557a9811f1554fb2c0ad5
Matthew Noyer <Matthew.noyer@kearsarge.org>
2019-01-03 08:25:12 -0500

    levels and menu

--- Screens.py
+++ Screens.py
@@ -1,1 +1,42 @@
-Scoreboard
+
+
+def showSplash():
+
+
+
+
+
+
+
+
+def getMenuInput():
+    goodInput = False
+    while not goodInput:
+        response = raw_input(" > ")
+        if (response == "1" 
+            or response == "One"):
+            response = "1"
+            goodInput = True
+        elif (response == "2" 
+            or response == "Two"):
+            response = "2"
+            goodInput = True
+        elif (response == "3"
+            or response == "Three"):
+            response = "3"
+            goodInput = True
+        elif (response == "4"
+            or response == "Four"):
+            response = "4"
+            goodInput = True
+        elif (response == "Q"
+              or response == "Quit"
+              or response == "q"
+              or response == "quit"
+              or response == "X"
+              or response == "Exit"):
+              response = "Q"
+              goodInput = True              
+        else:
+            print "Please make a valid choice boi"
+    return response

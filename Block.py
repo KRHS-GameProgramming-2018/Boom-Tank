b4f135095c73fe2615cddc50317adff3757ade83
Meg Roy <megan.roy@kearsarge.org>
2019-01-11 07:47:30 -0500

    wood background

--- Block.py
+++ Block.py
@@ -2,7 +2,7 @@ import pygame, sys, math
 
 class Block():
     def __init__(self,  pos=[6,0]):
-        self.image = pygame.image.load("Blocks/wall.png")
+        self.image = pygame.image.load("block.png")
         self.rect = self.image.get_rect(center=pos)
         self.radius = (self.rect.width/2 + self.rect.height/2)/2
 

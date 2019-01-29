f08124f72ab459fabae7940f6de396c65a51ae8e
Matthew Noyer <Matthew.noyer@kearsarge.org>
2019-01-29 08:19:31 -0500

    working on bullet and shoot

--- Bullet.py
+++ Bullet.py
@@ -46,5 +46,47 @@ class bullet:
             self.speedy = 0
         self.speed = [self.speedx, self.speedy]
         self.rect = self.rect.move(self.speed)
+        
+    def bounceWall(self, size):
+        width = size[0]
+        height = size[1]
+        if self.rect.left < 0 or self.rect.right > width:
+            if not self.didBounceX:
+                self.speedx = -self.speedx
+                self.didBounceX = False
+        if self.rect.top < 0 or self.rect.bottom > height:
+            if not self.didBounceY:
+                self.speedy = -self.speedy
+                self.didBounceY = False   
+        
+    def collide(self, other):
+        if not(self == other):
+            if self.rect.right > other.rect.left:
+                if self.rect.left < other.rect.right:
+                    if self.rect.top < other.rect.bottom:
+                        if self.rect.bottom > other.rect.top:
+                            if self.radius + other.radius > self.getDist(other.rect.center):
+                                if not self.didBounceX:
+                                    if self.speedx > 1: #right
+                                        if self.rect.centerx < other.rect.centerx:
+                                            self.speedx = 0
+                                            self.didBounceX = True
+                                    if self.speedx < 1: #left
+                                        if self.rect.centerx > other.rect.centerx:
+                                            self.speedx = 0
+                                            self.didBounceX = True
+                                            
+                                if not self.didBounceY:
+                                    if self.speedy > 1: #down
+                                        if self.rect.centery < other.rect.centery:
+                                            self.speedy = 0
+                                            self.didBounceY = True
+                                    if self.speedy < 1: #up
+                                        if self.rect.centery > other.rect.centery:
+                                            self.speedy  = 0
+                                            self.didBounceY = True
+
+                                return True
+        return False
     
     

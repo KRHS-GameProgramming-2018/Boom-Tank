93ce0554a49eb2318f746ed2bb6d09a07387a031
andrewLagos2020 <andrew.lagos@kearsarge.org>
2019-04-16 08:13:14 -0400

    Start screen and buttons

---
+++ Button.py
@@ -0,0 +1,33 @@
+import pygame, sys, math
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, kind, pos=[0,0]):
+        pygame.sprite.Sprite.__init__(self, self.containers)
+        if kind == "start":
+            self.basicImage = pygame.image.load("Images/Buttons/StartButton.png")
+            self.hoverImage = pygame.image.load("Images/Buttons/StartButtonHover.png")
+            self.clickImage = pygame.image.load("Images/Buttons/StartButtonClicked.png")
+        self.image = self.basicImage
+        self.rect = self.image.get_rect(center=pos)
+        self.radius = (self.rect.width/2 + self.rect.height/2)/2
+        
+    def collidePt(self, pt):
+        if self.rect.right > pt[0]:
+            if self.rect.left < pt[0]:
+                if self.rect.top < pt[1]:
+                    if self.rect.bottom > pt[1]:
+                        return True
+        return False
+        
+    def checkHover(self, pt):
+        if self.collidePt(pt):
+            self.image = self.hoverImage
+        else:
+            self.image = self.basicImage
+            
+    def checkClick(self, pt):
+        if self.collidePt(pt):
+            self.image = self.clickImage
+        else:
+            self.image = self.basicImage
+
